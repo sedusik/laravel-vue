201bd03b4f81d6fbe98966ae6d99d285338e819f
sedusik <sedusova.tanya@yandex.ru>
2024-06-20 02:22:16 +0300

    import component in component

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
-        1111
+        <SinglePostComponent></SinglePostComponent>
     </div>
 </template>
 
 <script>
+import SinglePostComponent from "@/components/SinglePostComponent.vue";
 export default {
-    name: "PostComponent"
+    name: "PostComponent",
+    components: {SinglePostComponent}
 }
 </script>
 
