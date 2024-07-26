7399d42c7b9be6154ca70107b1244e69017fa11f
sedusik <sedusova.tanya@yandex.ru>
2024-07-26 04:09:49 +0300

    asynchronous CRUD. Updating the persona. #2 Axios update request

--- resources/js/components/IndexComponent.vue
+++ resources/js/components/IndexComponent.vue
@@ -11,20 +11,20 @@
             </tr>
             </thead>
             <tbody>
-            <template v-for="person in people">
-                <tr>
+            <template v-for="person in people" >
+                <tr :class="isEdit(person.id) ? 'd-none' : ''">
                     <th scope="row">{{ person.id }}</th>
                     <td>{{ person.name }}</td>
                     <td>{{ person.age }}</td>
                     <td>{{ person.job }}</td>
-                    <td><a href="#" @click.prevent="changeEditPersonId(person.id)" class="btn btn-success">Редактировать</a></td>
+                    <td><a href="#" @click.prevent="changeEditPersonId(person.id, person.name, person.age, person.job)" class="btn btn-success">Редактировать</a></td>
                 </tr>
                 <tr :class="isEdit(person.id) ? '' : 'd-none'">
                     <th scope="row">{{ person.id }}</th>
-                    <td><input type="text" class="form-control"></td>
-                    <td><input type="number" class="form-control"></td>
-                    <td><input type="text" class="form-control"></td>
-                    <td><a href="#" @click.prevent="changeEditPersonId(null)" class="btn btn-dark">Обновить</a></td>
+                    <td><input type="text" v-model="name" class="form-control"></td>
+                    <td><input type="number" v-model="age" class="form-control"></td>
+                    <td><input type="text" v-model="job" class="form-control"></td>
+                    <td><a href="#" @click.prevent="updatePerson(person.id)" class="btn btn-dark">Обновить</a></td>
                 </tr>
             </template>
             </tbody>
@@ -40,7 +40,10 @@ export default {
     data() {
         return {
             people: null,
-            editPersonId: null
+            editPersonId: null,
+            name: null,
+            age: null,
+            job: '',
         }
     },
 
@@ -56,12 +59,23 @@ export default {
                 })
         },
 
-        changeEditPersonId(id) {
+        changeEditPersonId(id, name, age, job) {
             this.editPersonId = id
+            this.name = name
+            this.age = age
+            this.job = job
         },
 
         isEdit(id) {
             return this.editPersonId === id
+        },
+
+        updatePerson(id) {
+            this.editPersonId = null
+            axios.patch(`/api/people/${id}`, {name: this.name, age: this.age, job: this.job})
+                .then( res => {
+                    this.getPeople()
+                })
         }
     }
 }
